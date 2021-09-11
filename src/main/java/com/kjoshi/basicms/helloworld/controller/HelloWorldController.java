a4c32090dd7fdc58451577ef322357939083940e
Keyur Joshi <keyurjoshi@Keyurs-MacBook-Pro.local>
2021-09-11 10:15:30 +1000

    Minor changes

--- src/main/java/com/kjoshi/basicms/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/kjoshi/basicms/helloworld/controller/HelloWorldController.java
@@ -2,19 +2,23 @@ package com.kjoshi.basicms.helloworld.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/hello-world")
+@RequestMapping("${app.env.baseurl}")
 @RestController
 public class HelloWorldController {
+
+    @Value("${app.env.name}")
+    private String envName;
 
     private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
     @RequestMapping("/hello")
     public String sayHello(){
         logger.info("Hello called");
-        return "SUCCESS";
+        return "Hello "+envName;
     }
 
 }
